cdbaae03f94c23e799233356146277efc505a17d
Erdi Kose <me@erdkse.com>
2020-04-27 22:22:42 +0300

    add loading

---
+++ src/components/page-loading/PageLoading.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './PageLoading.module.scss';
+
+const PageLoading = () => {
+  return (
+    <div className={classes.loading}>
+      <span>A</span>
+      <span>D</span>
+      <span>M</span>
+      <span>I</span>
+      <span>N</span>
+      <span>L</span>
+      <span>T</span>
+      <span>E</span>
+    </div>
+  );
+};
+
+export default PageLoading;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/index';
