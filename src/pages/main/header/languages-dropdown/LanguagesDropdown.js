d221f6f4b24026a8880c4d2cea18e363e864d9a0
Erdi Köse <me@erdkse.com>
2020-12-16 03:20:26 +0300

    handle language changes

--- src/pages/main/header/languages-dropdown/LanguagesDropdown.js
+++ src/pages/main/header/languages-dropdown/LanguagesDropdown.js
@@ -1,16 +1,44 @@
 import React, {useRef, useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 
+const languages = [
+    {
+        key: 'en',
+        icon: 'flag-icon-us',
+        label: 'header.language.english'
+    },
+    {
+        key: 'tr',
+        icon: 'flag-icon-tr',
+        label: 'header.language.turkish'
+    },
+    {
+        key: 'de',
+        icon: 'flag-icon-de',
+        label: 'header.language.german'
+    },
+    {
+        key: 'fr',
+        icon: 'flag-icon-fr',
+        label: 'header.language.french'
+    },
+    {
+        key: 'es',
+        icon: 'flag-icon-es',
+        label: 'header.language.spanish'
+    }
+];
+
 const LanguagesDropdown = () => {
     const dropdownRef = useRef(null);
     const {t, i18n} = useTranslation();
 
-    const [dropdownState, updateDropdownState] = useState({
+    const [dropdownState, setDropdownState] = useState({
         isDropdownOpen: false
     });
 
     const toggleDropdown = () => {
-        updateDropdownState({isDropdownOpen: !dropdownState.isDropdownOpen});
+        setDropdownState({isDropdownOpen: !dropdownState.isDropdownOpen});
     };
 
     const handleClickOutside = (event) => {
@@ -19,7 +47,7 @@ const LanguagesDropdown = () => {
             dropdownRef.current &&
             !dropdownRef.current.contains(event.target)
         ) {
-            updateDropdownState({isDropdownOpen: false});
+            setDropdownState({isDropdownOpen: false});
         }
     };
 
@@ -44,60 +72,47 @@ const LanguagesDropdown = () => {
         i18n.changeLanguage(lng);
     };
 
+    const getCurrentLanguage = () => {
+        const currentLanguage = i18n.language;
+        if (currentLanguage) {
+            return languages.find(
+                (language) => language.key === currentLanguage
+            );
+        }
+        return {};
+    };
+
+    const isActiveLanguage = (language) => {
+        if (language) {
+            return getCurrentLanguage().key === language.key ? 'active' : '';
+        }
+        return '';
+    };
+
     return (
         <li
             ref={dropdownRef}
             className="nav-item d-none d-sm-inline-block dropdown"
         >
-            <button
-                onClick={toggleDropdown}
-                type="button"
-                className="nav-link"
-                data-toggle="dropdown"
-            >
-                <i className="flag-icon flag-icon-tr" />
+            <button onClick={toggleDropdown} type="button" className="nav-link">
+                <i className={`flag-icon ${getCurrentLanguage().icon}`} />
             </button>
             <div className={className}>
-                <button
-                    type="button"
-                    className="dropdown-item active"
-                    onClick={() => changeLanguage('tr')}
-                >
-                    <i className="flag-icon flag-icon-tr mr-2" />
-                    <span>{t('header.language.turkish')}</span>
-                </button>
-                <button
-                    type="button"
-                    className="dropdown-item"
-                    onClick={() => changeLanguage('en')}
-                >
-                    <i className="flag-icon flag-icon-us mr-2" />
-                    <span>{t('header.language.english')}</span>
-                </button>
-                <button
-                    type="button"
-                    className="dropdown-item"
-                    onClick={() => changeLanguage('de')}
-                >
-                    <i className="flag-icon flag-icon-de mr-2" />
-                    <span>{t('header.language.german')}</span>
-                </button>
-                <button
-                    type="button"
-                    className="dropdown-item"
-                    onClick={() => changeLanguage('fr')}
-                >
-                    <i className="flag-icon flag-icon-fr mr-2" />
-                    <span>{t('header.language.french')}</span>
-                </button>
-                <button
-                    type="button"
-                    className="dropdown-item"
-                    onClick={() => changeLanguage('es')}
-                >
-                    <i className="flag-icon flag-icon-es mr-2" />
-                    <span>{t('header.language.spanish')}</span>
-                </button>
+                {languages.map((language) => (
+                    <button
+                        type="button"
+                        className={`dropdown-item ${isActiveLanguage(
+                            language
+                        )}`}
+                        onClick={() => {
+                            changeLanguage(language.key);
+                            setDropdownState(false);
+                        }}
+                    >
+                        <i className={`flag-icon ${language.icon} mr-2`} />
+                        <span>{t(language.label)}</span>
+                    </button>
+                ))}
             </div>
         </li>
     );
