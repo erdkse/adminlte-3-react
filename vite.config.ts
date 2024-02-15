ae804f70fd77a1846fd8d415938ecb58086a7807
Erdi Köse <me@erdkse.com>
2024-02-15 22:25:51 +0300

    chore: add vercel rewrite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  mode: 'development',
-  plugins: [react(), ],
-  resolve: {
-    alias: {
-      '@app': path.resolve(__dirname, './src'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@modules': path.resolve(__dirname, './src/modules'),
-      '@pages': path.resolve(__dirname, './src/pages'),
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    mode: 'development',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@app': path.resolve(__dirname, './src'),
+        '@store': path.resolve(__dirname, './src/store'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@modules': path.resolve(__dirname, './src/modules'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+      },
     },
-  },
-});
+  });
+};
