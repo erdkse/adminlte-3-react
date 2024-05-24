f5915d497f49d9e98dcda61e4d8469545906e827
Erdi Köse <me@erdkse.com>
2024-05-24 23:44:41 +0300

    chore: needless code pieces

--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -1,14 +1,11 @@
 import { IUser } from '@app/types/user';
 import { createSlice } from '@reduxjs/toolkit';
-import { User } from 'oidc-client-ts';
 
 export interface AuthState {
-  authentication?: User;
   currentUser?: IUser | null;
 }
 
 const initialState: AuthState = {
-  authentication: undefined,
   currentUser: undefined,
 };
 
@@ -16,15 +13,12 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setAuthentication: (state: any, { payload }: any) => {
-      state.authentication = payload;
-    },
     setCurrentUser: (state: any, { payload }: any) => {
       state.currentUser = payload;
     },
   },
 });
 
-export const { setAuthentication, setCurrentUser } = authSlice.actions;
+export const { setCurrentUser } = authSlice.actions;
 
 export default authSlice.reducer;
