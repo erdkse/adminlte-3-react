e45353dd59a9a28aa837127e5ade1af198fab7bb
Erdi Köse <me@erdkse.com>
2024-05-24 05:29:28 +0300

    feat: add loading state to info box component

--- src/components/info-box/InfoBox.tsx
+++ src/components/info-box/InfoBox.tsx
@@ -6,18 +6,23 @@ type VARIANT_TYPES =
   | 'warning'
   | 'danger'
   | 'success'
-  | 'info';
+  | 'info'
+  | 'dark'
+  | 'light';
 
 export const InfoBox = ({
-  fillBackground,
   variant,
   title,
   text,
   progressBar,
   icon,
+  loading,
 }: {
-  fillBackground?: boolean;
-  icon?: ReactNode;
+  loading?: 'dark' | boolean;
+  icon?: {
+    content: ReactNode;
+    variant?: VARIANT_TYPES;
+  };
   variant?: VARIANT_TYPES;
   title: string;
   text: string;
@@ -27,17 +32,14 @@ export const InfoBox = ({
     variant?: VARIANT_TYPES;
   };
 }) => {
-  variant = variant || 'primary';
-
   const progressBarContent = useMemo(() => {
     if (progressBar) {
       const proggressBarVariant = progressBar.variant || variant;
-      const progressBarClassName = `progress-bar ${progressBar.variant ? `bg-${proggressBarVariant}` : ''}`; 
       return (
         <>
           <div className="progress">
             <div
-              className={progressBarClassName}
+              className={`progress-bar ${proggressBarVariant ? `bg-${proggressBarVariant}` : ''}`}
               style={{ width: `${progressBar?.level || 0}%` }}
             />
           </div>
@@ -52,17 +54,37 @@ export const InfoBox = ({
     return;
   }, [progressBar, variant]);
 
-  return (
-    <div className={`info-box ${fillBackground ? `bg-${variant}` : ''}`}>
-      <span className={`info-box-icon ${fillBackground ? '' : `bg-${variant}`}`}>
-        {icon || <i className="far fa-envelope" />}
-      </span>
+  const iconContent = useMemo(() => {
+    const iconVariant = icon?.variant || variant;
 
+    return (
+      <span
+        className={`info-box-icon ${iconVariant ? `bg-${iconVariant}` : ''}`}
+      >
+        {icon?.content || <i className="far fa-envelope" />}
+      </span>
+    );
+  }, [icon]);
+
+  function hasTheme(
+    loading: boolean | { theme?: 'dark' }
+  ): loading is { theme?: 'dark' } {
+    return (loading as { theme?: 'dark' }).theme !== undefined;
+  }
+
+  return (
+    <div className={`info-box ${variant ? `bg-${variant}` : ''}`}>
+      {iconContent}
       <div className="info-box-content">
         <span className="info-box-text">{title}</span>
         <span className="info-box-number">{text}</span>
         {progressBarContent}
       </div>
+      {loading && (
+        <div className={`overlay ${typeof loading === 'string' && loading}`}>
+          <i className="fas fa-2x fa-sync-alt fa-spin"></i>
+        </div>
+      )}
     </div>
   );
 };
